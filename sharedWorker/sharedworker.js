2beda42751722574bc449a265507fd7564a33207
unknown <nkemakolam.martin@gmail.com>
2023-08-12 21:49:22 +0100

    sharedworkers

--- sharedWorker/sharedworker.js
+++ sharedWorker/sharedworker.js
@@ -1,1 +1,10 @@
-
+let pages = []
+onconnect=evt=>{
+    let port = evt.ports[0]
+    pages.push(port)
+    pages.forEach((ports)=>{
+        ports.onmessage=(e)=>{
+            port.postMessage(pages.length)
+        }
+    })
+}
